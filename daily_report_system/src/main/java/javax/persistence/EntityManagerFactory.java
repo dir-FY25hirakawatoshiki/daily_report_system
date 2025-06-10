be472b6afff361e549f15816898bedff7a6d059e
ToshikiHirakawa(平川季樹 <toshiki.hirakawa.a780@dirbato.co.jp>
2025-06-10 10:18:02 +0900

    「DBUtil作成」

---
+++ daily_report_system/src/main/java/javax/persistence/EntityManagerFactory.java
@@ -0,0 +1,10 @@
+package javax.persistence;
+
+public record EntityManagerFactory() {
+
+    public EntityManager createEntityManager() {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+}
---
+++ daily_report_system/src/main/java/javax/persistence/Persistence.java
@@ -0,0 +1,10 @@
+package javax.persistence;
+
+public record Persistence() {
+
+    public static EntityManagerFactory createEntityManagerFactory(String persistenceUnitName) {
+        // TODO 自動生成されたメソッド・スタブ
+        return null;
+    }
+
+}
